f26621979974af5d9e35fcba7a64365109573fa3
Musanna Abdullah Bayezid <42944621+MBayezid@users.noreply.github.com>
2025-10-04 11:08:12 -0700

    Remove unused markdown files and add secrets.h for WiFi credentials

---
+++ src/secrets.h
@@ -0,0 +1,13 @@
+
+ // Load WIFI_PASS configuration/Keys/file or environment variable instead doing this
+// Concedering my initial goals .env/prescripting py or CI/CD not a relevent things right now.
+//  trying to focus on hardware constrains for some versions..
+
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID "MB"
+
+#define WIFI_PASS "mayesha2k10"
+
+#endif
